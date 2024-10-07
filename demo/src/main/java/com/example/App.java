71597ddae8140b1285b34d3e553884ba30b4bc37
aaron <aaronrvaraw@gmail.com>
2024-10-07 11:41:42 +0800

    Using JxBrowser

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.view.javafx.BrowserView;
+import com.teamdev.jxbrowser.engine.RenderingMode;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -10,16 +17,42 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        WebView webView = new WebView();
-        WebEngine webEngine = webView.getEngine();
+        /*
+         * WebView webView = new WebView();
+         * WebEngine webEngine = webView.getEngine();
+         * 
+         * // Load the HTML file from resources
+         * 
+         * webEngine.load(App.class.getResource("index.html").toExternalForm());
+         */
+        // Initialize Chromium.
+        EngineOptions options = EngineOptions.newBuilder(RenderingMode.HARDWARE_ACCELERATED)
+                .licenseKey(
+                        "3GC4U6A49ZRMTY18LZW7ZC38WH3GH616N56D9C0XYMRRBQI1YSMJFHMUE6DZ93AQBLRMHJHHB8DVI9E3YN57EGGPUJJFM4XHAE48SGNMERVOAI450A5TR13PEBP9T07Y9DAVON7E9XKU7VEN")
+                .build();
+        Engine engine = Engine.newInstance(options);
 
-        // Load the HTML file from resources
+        // Create a Browser instance.
+        Browser browser = engine.newBrowser();
 
-        webEngine.load(App.class.getResource("index.html").toExternalForm());
+        // Load the required web page.
+        browser.navigation().loadUrl("https://html5test.teamdev.com");
 
-        Scene scene = new Scene(webView, 800, 600);
+        // Create and embed JavaFX BrowserView component to display web content.
+        BrowserView view = BrowserView.newInstance(browser);
+
+        Scene scene = new Scene(new BorderPane(view), 1280, 800);
+        primaryStage.setTitle("JxBrowser JavaFX");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // Shutdown Chromium and release allocated resources.
+        primaryStage.setOnCloseRequest(event -> engine.close());
+        /*
+         * Scene scene = new Scene(webView, 800, 600);
+         * primaryStage.setScene(scene);
+         * primaryStage.show();
+         */
     }
 
     public static void main(String[] args) {
--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires jxbrowser;
+    requires jxbrowser.javafx;
 
     exports com.example;
 }
