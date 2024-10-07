3a09bc48aa4fa4a499288eeb697076c0e2392425
Balongie <butiubalong@gmail.com>
2024-10-08 02:32:40 +0800

    added FirstWindow.fxl, updated DialogController.fxml

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -23,53 +23,50 @@ public class App extends Application {
          * only thing to do here is to call JavaFX Components and add Children
          * ====Hierarchy====
          * HBox (root)
-         *   VBox (OpeningScreenController)
-         *   ImageView
-         *   Label
-         *   HBox
-         *     Checkbox
-         *     Label
-         *   Button
-         *  BorderPane <1st Controller> (To be added are the video api which has <3rd Controller>) /
-         *  VBox (InfoPanel) <2nd Controller>
-         *      HBox (WeatherField)
-         *      [Add to here later]
-         *  HBox (SearchField)
-         *      TextField
-         *      Button
-         *  VBox (SuggestionCtr)
-         *      Label (Places near you)
-         *      HBox
-         *      Label (Tags Label)
-         *      Label (Filters (For Loop))
-         *  ListView
-         *      HBox
-         *      ImageView
-         *      VBox
-         *          Label (Title)
-         *          HBox (Rating)
-         *          Label (RatingsText)
-         *          Label (Stars)
-         *          Label (Address) (Opening Hours??)
-         *      Label (Filters (For Loop))
+         * VBox (OpeningScreenController)
+         * ImageView
+         * Label
+         * HBox
+         * Checkbox
+         * Label
+         * Button
+         * BorderPane <1st Controller> (To be added are the video api which has <3rd
+         * Controller>) /
+         * VBox (InfoPanel) <2nd Controller>
+         * HBox (WeatherField)
+         * [Add to here later]
+         * HBox (SearchField)
+         * TextField
+         * Button
+         * VBox (SuggestionCtr)
+         * Label (Places near you)
+         * HBox
+         * Label (Tags Label)
+         * Label (Filters (For Loop))
+         * ListView
+         * HBox
+         * ImageView
+         * VBox
+         * Label (Title)
+         * HBox (Rating)
+         * Label (RatingsText)
+         * Label (Stars)
+         * Label (Address) (Opening Hours??)
+         * Label (Filters (For Loop))
          */
 
         // Declaring Scenes
         HBox root = new HBox();
-        FXMLLoader mapLoader = new FXMLLoader(App.class.getResource("MapView.fxml"));
-        Parent pane = mapLoader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("DialogController.fxml"));
+        Parent pane = fxmlLoader.load();
 
-        MapController mapController = mapLoader.getController();
+        MapController mapController = fxmlLoader.getController();
 
         primaryStage.setOnCloseRequest(event -> {
             if (mapController.engine != null) {
                 mapController.engine.close(); // Close the engine when the window is closing
             }
         });
-
-        
-
-
 
         /*
          * VBox infoPanel = (VBox) loadFXML("InfoPanelView");
